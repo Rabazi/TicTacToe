dd659f4b59cff46924d6df3a0b2daf81d616ce10
Rabazi <61808507+Rabazi@users.noreply.github.com>
2022-01-26 17:49:18 +0100

    Setting buttons to free

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace TicTacToe
 {
@@ -8,9 +10,28 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MarkType[] markTypes;
+        private bool Player1Turn;
+        private bool GameEnd;
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            markTypes = new MarkType[9];
+
+            for (var i = 0; i < markTypes.Length; i++)
+                markTypes[i] = MarkType.Free;
+            Player1Turn = true;
+            // Iterate every button
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+
+            });
         }
     }
 }
